787b44b73199937dd913555d5aedbbabd45f7909
Brianna Root <brianna.k.root@gmail.com>
2024-10-01 20:06:31 -0600

    created functions to class Plant, then created tests.

--- classes/plant.py
+++ classes/plant.py
@@ -1,4 +1,35 @@
 class Plant:
 
-# Write your plant class here!
-    pass
+    def __init__(self, name, water_level, sunlight_hours, is_blooming=False):
+        self.name = name
+        self. water_level = water_level
+        self.sunlight_hours = sunlight_hours
+        self.is_blooming = is_blooming
+
+
+    def water(self, water_amount):
+        self.water_level += water_amount 
+        
+        return f"{self.name} new water level: {self.water_level}"
+    
+    def give_sunlight(self, hours):
+        self.sunlight_hours += hours
+
+        return f"{self.name} new sunlight hours: {self.sunlight_hours}"
+    
+    # using attributes to produce new data 
+    def check_growth(self):
+        if self.sunlight_hours > 5 and self.water_level > 5:
+            return "mature"
+        else:
+            return "sprout"
+
+
+plant = Plant("Butterfly pear flower", 6, 7)
+
+print(plant.check_growth())
+
+# print(plant.name, plant.water_level, plant.sunlight_hours, plant.is_blooming)
+
+# can rearrange the names:
+# print(plant.name, plant.is_blooming, plant.sunlight_hours, plant.water_level)
--- tests/test_plant.py
+++ tests/test_plant.py
@@ -1,1 +1,53 @@
 from classes.plant import Plant
+
+
+def test_plant_initialization():
+    
+    # Arrange
+    name = "Butterfly pear flower"
+    water_level = 7
+    sunlight_hours = 6
+    is_blooming = True
+
+    # Act
+    plant = Plant(name, water_level, sunlight_hours, is_blooming)
+
+    plant_2 = Plant(name, water_level, sunlight_hours)
+
+    # Assert
+    assert plant.name == name 
+    assert plant.water_level == water_level 
+    assert plant.sunlight_hours == sunlight_hours 
+    assert plant.is_blooming  # don't need to check against it because it is True
+
+
+    assert not plant_2.is_blooming # because fasly should result to True
+
+
+def test_plant_watering():
+
+    # Arrange
+    amount = 3
+    air_plant = Plant("Air Plant", 0, 8, False)
+    expected = 3
+
+    # Act
+    result = air_plant.water(amount)
+    # Assert
+    assert result == f"Air Plant new water level: {expected}"
+    assert air_plant.water_level == expected
+
+
+def test_growth_stage_after_watering():
+    # Arrange
+    baby_tears = Plant("Baby Tears", 3, 6, True)
+    amount = 3
+
+    # Act
+    before_water = baby_tears.check_growth()
+    baby_tears.water(amount)
+    after_water = baby_tears.check_growth()
+
+    # Assert
+    assert before_water == "sprout"
+    assert after_water == "mature"
